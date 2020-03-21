e368031ac9da8139a1127b335344e7a2b028596a
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-21 16:10:58 -0300

    Implementado o metodo findAll nas classes CategoriaService e CategoriaController

---
+++ src/main/java/com/controllers/CategoriaController.java
@@ -0,0 +1,27 @@
+package com.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.models.Categoria;
+import com.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaService _service;
+
+	@GetMapping
+	public ResponseEntity<List<Categoria>> findAll() {
+		List<Categoria> list = _service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
