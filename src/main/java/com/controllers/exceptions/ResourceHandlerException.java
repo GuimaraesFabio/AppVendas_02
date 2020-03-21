60e272ec0fa163afda701cc0de46e30ac8dd3473
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-21 17:06:55 -0300

    Implementado o metodo findById nas classes CategoriaService e CategoriaController

---
+++ src/main/java/com/controllers/exceptions/ResourceHandlerException.java
@@ -0,0 +1,26 @@
+package com.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceHandlerException {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNoFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		String error = "Objeto não encontrado.";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
