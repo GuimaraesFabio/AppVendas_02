fb5e6163c17c2112dfac85fbd50a9ba79ab59653
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-22 14:49:21 -0300

    Implementado as configurações para rodar ewm prod

--- src/main/java/com/models/Cliente.java
+++ src/main/java/com/models/Cliente.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -31,7 +30,6 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	@Column(unique = true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
