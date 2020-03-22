37d9ad797d3473422b9fd1316fcd1ab1b2e235cd
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-22 10:49:26 -0300

    Implementado o metodo por busca paginada

--- src/main/java/com/controllers/CategoriaController.java
+++ src/main/java/com/controllers/CategoriaController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dtos.CategoriaDto;
@@ -32,6 +34,17 @@ public class CategoriaController {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = _service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDto>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linePerPage", defaultValue = "24") Integer linePerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
+
+		Page<Categoria> list = _service.findPage(page, linePerPage, direction, orderBy);
+		Page<CategoriaDto> listDto = list.map(obj -> new CategoriaDto(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/services/CategoriaService.java
+++ src/main/java/com/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.models.Categoria;
@@ -24,5 +27,11 @@ public class CategoriaService {
 
 		Optional<Categoria> obj = _repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado."));
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linePerPage, String direction, String orderBy) {
+
+		PageRequest pr = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return _repository.findAll(pr);
 	}
 }
