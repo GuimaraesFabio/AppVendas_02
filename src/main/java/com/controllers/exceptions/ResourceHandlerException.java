79d9a8d3df18e77346f5c2332494f3c66e1d8a8c
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-22 11:17:49 -0300

    Implementado o metodo ValidatorError (CategoriaController)

--- src/main/java/com/controllers/exceptions/ResourceHandlerException.java
+++ src/main/java/com/controllers/exceptions/ResourceHandlerException.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +23,22 @@ public class ResourceHandlerException {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidatorError> validatorError(MethodArgumentNotValidException e,
+			HttpServletRequest request) {
+
+		String error = "Erro de validação.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ValidatorError err = new ValidatorError(Instant.now(), status.value(), error, error, request.getRequestURI());
+
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+
+			err.addErrors(x.getField(), x.getDefaultMessage());
+		}
+
 		return ResponseEntity.status(status).body(err);
 	}
 }
