068f167179747c969290fbbac3851da79194d43b
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-22 12:22:07 -0300

    Criado e implementado todo o processo a inserção de um novo cliente (Cliente)

--- src/main/java/com/controllers/ClienteController.java
+++ src/main/java/com/controllers/ClienteController.java
@@ -1,14 +1,26 @@
 package com.controllers;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.dtos.ClienteDto;
+import com.dtos.ClienteNewDto;
 import com.models.Cliente;
 import com.services.ClienteService;
 
@@ -29,6 +41,39 @@ public class ClienteController {
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
 		Cliente obj = _service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDto>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linePerPage", defaultValue = "24") Integer linePerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
+
+		Page<Cliente> list = _service.findPage(page, linePerPage, direction, orderBy);
+		Page<ClienteDto> listDto = list.map(obj -> new ClienteDto(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDto objDto) {
+		Cliente obj = _service.fromDto(objDto);
+		obj = _service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDto objDto, @PathVariable Integer id) {
+		Cliente obj = _service.fromDto(objDto);
+		obj.setId(id);
+		_service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		_service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/services/ClienteService.java
+++ src/main/java/com/services/ClienteService.java
@@ -4,10 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dtos.ClienteDto;
+import com.dtos.ClienteNewDto;
+import com.enums.TipoCliente;
+import com.models.Cidade;
 import com.models.Cliente;
+import com.models.Endereco;
+import com.repositories.CidadeRepository;
 import com.repositories.ClienteRepository;
+import com.repositories.EnderecoRepository;
+import com.services.exceptions.DataIntegrityException;
 import com.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,6 +28,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository _repository;
+
+	@Autowired
+	private CidadeRepository _cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository _enderecoRepository;
 
 	public List<Cliente> findAll() {
 		return _repository.findAll();
@@ -25,4 +44,69 @@ public class ClienteService {
 		Optional<Cliente> obj = _repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado."));
 	}
+
+	public Page<Cliente> findPage(Integer page, Integer linePerPage, String direction, String orderBy) {
+
+		PageRequest pr = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return _repository.findAll(pr);
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		if (obj.getId() == null) {
+			_repository.save(obj);
+			_enderecoRepository.saveAll(obj.getEnderecos());
+		}
+		return obj;
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return _repository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+
+		findById(id);
+		try {
+			_repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Erro ao excluir, existem relacionamento entre a categoria e demais entidades.");
+		}
+
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public Cliente fromDto(ClienteDto objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDto(ClienteNewDto objDto) {
+
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = _cidadeRepository.getOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+
+		cli.getTelefones().add(objDto.getTelefone1());
+		cli.getEnderecos().add(end);
+
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
+	}
+
 }
